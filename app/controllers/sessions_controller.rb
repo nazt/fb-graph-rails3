540b5ad98b191f71714bb1f365607f0c8f828200
Nat <nat.weerawan@gmail.com>
2010-11-23 22:41:00 +0700

    FbGraph

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   def create
     @rack_auth = request.env['rack.auth']
-    #render :text => @rack_auth.inspect
+    render :text => @rack_auth.inspect
     #render :text => FbGraph:User.me('125770127482918|a74455a00bb7134df96a7e84-896050346|JNDdXhCkwprKtO5k1p7rW0yVvtY').inspect
-    render :text => FbGraph.inspect
+    #render :text => FbGraph.inspe
   end
 end
