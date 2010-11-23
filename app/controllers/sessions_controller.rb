e208e5c5585587f5651454d55918164529310be6
Nat <nat.weerawan@gmail.com>
2010-11-23 22:59:35 +0700

    token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     client = FBGraph::Client.new(:client_id => '125770127482918',:secret_id =>'e7f29e986d7570848c69257f3c03cb7e', :token =>@token)
     #client.inspect
     
-    render :text => client.selection.me.photos.until(Time.now.to_s).since(3.days.ago).limit(10).info client.selection.user('id').videos.offset(10).inspect
+    render :text => client.selection.me.photos.until(Time.now.to_s).since(3.days.ago).limit(10).inspect
     
     #render :text => FbGraph:User.me('125770127482918|a74455a00bb7134df96a7e84-896050346|JNDdXhCkwprKtO5k1p7rW0yVvtY').inspect
     #render :text => FbGraph.inspe
